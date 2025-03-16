1fcacd962d60146a897444dd9fad4ba243f34574
emmaimade <emmaimade14@gmail.com>
2025-03-16 22:42:43 +0100

    Add Error Handlers

--- index.js
+++ index.js
@@ -9,20 +9,33 @@ dotenv.config();
 const API_KEY = process.env.TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
+const ENDPOINTS = {
+    popular: "popular",
+    top: "top_rated",
+    playing: "now_playing",
+    upcoming: "upcoming",
+};
+
 // Function to fetch movies
-async function getmovies(endpoint){
+async function getmovies(type){
     try {
+        const endpoint = ENDPOINTS[type];
+
+        if (!endpoint) {
+            console.log("Invalid type! Use: popular, top, playing, upcoming");
+            return;
+        }
+
         const response = await axios.get(`${BASE_URL}/movie/${endpoint}`, {
             params: {
-                apiKey: API_KEY
+                api_key: API_KEY
             },
         });
 
-        const movies = response.data;
-        console.log(movies);
+        const movies = response.data.results;
 
         if (movies.length === 0) {
-            console.log("")
+            console.log("No movies found.");
         } else {
             movies.forEach((movie, index) => {
                 console.log(`${index + 1}: ${movie.title} (${movie.release_date})`);
@@ -37,10 +50,10 @@ async function getmovies(endpoint){
 program
     .version('1.0.0')
     .description('TMDB CLI')
-    .option("--type <type>", "Type of movies (popular, top_rated, now_playing, upcoming)")
+    .option("--type <type>", "Type of movies (popular, top, playing, upcoming)")
     .action((options) => {
         if (!options.type) {
-            console.log("Please specify a valid type of movie from popular, top_rated, now_playing, upcoming");
+            console.log("Please specify a valid type of movie from popular, top, playing, upcoming");
             return;
         }
         getmovies(options.type);
