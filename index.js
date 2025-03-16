9522c21765c560b62872a3f042f11b840acec7b1
emmaimade <emmaimade14@gmail.com>
2025-03-16 21:46:21 +0100

    Define CLI commands

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ dotenv.config();
 const API_KEY = process.env.TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
+// Function to fetch movies
 async function getmovies(endpoint){
     try {
         const response = await axios.get(`${BASE_URL}/movie/${endpoint}`, {
@@ -18,11 +19,32 @@ async function getmovies(endpoint){
         });
 
         const movies = response.data;
+        console.log(movies);
 
         if (movies.length === 0) {
             console.log("")
+        } else {
+            movies.forEach((movie, index) => {
+                console.log(`${index + 1}: ${movie.title} (${movie.release_date})`);
+            });
         }
     } catch (error) {
-        
+        console.log("Error fetching movies:", error);
     }
 }
+
+// Define the command-line interface
+program
+    .version('1.0.0')
+    .description('TMDB CLI')
+    .option("--type <type>", "Type of movies (popular, top_rated, now_playing, upcoming)")
+    .action((options) => {
+        if (!options.type) {
+            console.log("Please specify a valid type of movie from popular, top_rated, now_playing, upcoming");
+            return;
+        }
+        getmovies(options.type);
+    });
+
+// Parse the command-line arguments
+program.parse(process.argv);
